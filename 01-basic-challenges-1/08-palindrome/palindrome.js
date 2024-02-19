1dbe96e1c85ee6eafcc887b3dbf2eb4a0ccd2d56
Ramon-Carrillo <carrillojrramon@gmail.com>
2024-02-19 10:57:08 -0600

    finished 7-10 of 01-basic-challanges

--- 01-basic-challenges-1/08-palindrome/palindrome.js
+++ 01-basic-challenges-1/08-palindrome/palindrome.js
@@ -1,3 +1,7 @@
-function isPalindrome() {}
+function isPalindrome(str) {
+  let cleanStr = str.replace(/[^A-Za-z0-9]/g, "").toLowerCase();
+  let reversedStr = cleanStr.split("").reverse().join("");
+  return cleanStr === reversedStr;
+}
 
 module.exports = isPalindrome;
