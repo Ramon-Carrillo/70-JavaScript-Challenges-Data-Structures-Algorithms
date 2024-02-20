9fb498a37e9226ac383ba9ff778c98f85dc9d5fa
Ramon-Carrillo <carrillojrramon@gmail.com>
2024-02-19 23:32:44 -0600

    finished 6 and 7 of 02-basic-challenges

--- 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
+++ 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
@@ -1,3 +1,12 @@
-function areAllCharactersUnique() {}
+function areAllCharactersUnique(str) {
+  const charMap = {};
+  for (let char of str) {
+    if (charMap[char]) {
+      return false;
+    }
+    charMap[char] = 1;
+  }
+  return true;
+}
 
 module.exports = areAllCharactersUnique;
--- 02-basic-challenges-2/07-first-non-repeating/first-non-repeating.js
+++ 02-basic-challenges-2/07-first-non-repeating/first-non-repeating.js
@@ -1,3 +1,18 @@
-function findFirstNonRepeatingCharacter() {}
+function findFirstNonRepeatingCharacter(str) {
+  const charMap = {};
+  for (let char of str) {
+    if (charMap[char]) {
+      charMap[char]++;
+    } else {
+      charMap[char] = 1;
+    }
+  }
+  for (let char of str) {
+    if (charMap[char] === 1) {
+      return char;
+    }
+  }
+  return null;
+}
 
 module.exports = findFirstNonRepeatingCharacter;
