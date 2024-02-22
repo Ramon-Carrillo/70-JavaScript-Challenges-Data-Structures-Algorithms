09ddb0c885118a388b2091aadc6a5295f5284b84
Ramon-Carrillo <carrillojrramon@gmail.com>
2024-02-22 10:36:06 -0600

    finished 7-10 of 03-high-order-array-methods

--- 03-high-order-array-methods/09-password-validator/password-validator.js
+++ 03-high-order-array-methods/09-password-validator/password-validator.js
@@ -1,3 +1,5 @@
-function validatePassword() {}
+function validatePassword(str) {
+  return /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/.test(str);
+}
 
 module.exports = validatePassword;
--- 03-high-order-array-methods/10-find-missing-letter-refactor/find-missing-letter-refactor.js
+++ 03-high-order-array-methods/10-find-missing-letter-refactor/find-missing-letter-refactor.js
@@ -1,3 +1,13 @@
-function findMissingLetter() {}
+function findMissingLetter(arr) {
+  let missingLetter = "";
+  arr.forEach((letter, index) => {
+    if (index < arr.length - 1) {
+      if (letter.charCodeAt() + 1 !== arr[index + 1].charCodeAt()) {
+        missingLetter = String.fromCharCode(letter.charCodeAt() + 1);
+      }
+    }
+  });
+  return missingLetter;
+}
 
 module.exports = findMissingLetter;
