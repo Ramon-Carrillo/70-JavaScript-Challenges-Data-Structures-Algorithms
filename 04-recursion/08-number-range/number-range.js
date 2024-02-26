7a68babddec64d0573ead97ae31f59ed7bc8d9a7
Ramon-Carrillo <carrillojrramon@gmail.com>
2024-02-26 13:29:24 -0600

    finished 1-10 of 04-recursion

--- 04-recursion/08-number-range/number-range.js
+++ 04-recursion/08-number-range/number-range.js
@@ -1,3 +1,8 @@
-function numberRange() {}
+function numberRange(startNum, endNum) {
+  if (startNum === endNum) {
+    return [startNum];
+  }
+  return [startNum, ...numberRange(startNum + 1, endNum)];
+}
 
 module.exports = numberRange;
--- 04-recursion/10-permutations/permutations.js
+++ 04-recursion/10-permutations/permutations.js
@@ -1,3 +1,17 @@
-function permutations() {}
+function permutations(str) {
+  if (str.length === 0) return [""];
+  if (str.length === 1) return [str];
+  const result = [];
+
+  for (let i = 0; i < str.length; i++) {
+    const currentChar = str[i];
+    const remainingChars = str.slice(0, i) + str.slice(i + 1);
+    const remainingPermutations = permutations(remainingChars);
+    for (let j = 0; j < remainingPermutations.length; j++) {
+      result.push(currentChar + remainingPermutations[j]);
+    }
+  }
+  return result;
+}
 
 module.exports = permutations;
