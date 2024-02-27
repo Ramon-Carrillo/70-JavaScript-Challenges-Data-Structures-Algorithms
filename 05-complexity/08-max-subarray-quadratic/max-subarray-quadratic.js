03ea242aac84334f2ba61a924f1a42ff4e1c5b95
Ramon-Carrillo <carrillojrramon@gmail.com>
2024-02-27 11:55:44 -0600

    finished section 05-complexity

--- 05-complexity/08-max-subarray-quadratic/max-subarray-quadratic.js
+++ 05-complexity/08-max-subarray-quadratic/max-subarray-quadratic.js
@@ -1,3 +1,13 @@
-function maxSubarraySum() {}
+function maxSubarraySum(arr, k) {
+  let maxSum = -Infinity;
+  for (let i = 0; i <= arr.length - k; i++) {
+    let tempSum = 0;
+    for (let j = i; j < i + k; j++) {
+      tempSum += arr[j];
+    }
+    maxSum = Math.max(maxSum, tempSum);
+  }
+  return maxSum;
+}
 
 module.exports = maxSubarraySum;
