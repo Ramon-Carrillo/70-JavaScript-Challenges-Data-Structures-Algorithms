14e7ee6b8cc3c94b597004ed60bd02a121f3d7c1
Ramon-Carrillo <carrillojrramon@gmail.com>
2024-02-29 12:52:21 -0600

    finished 8-14 of 06-hash-tables-maps-sets

--- 06-hash-tables-maps-sets/08-symmetric-difference/symmetric-difference.js
+++ 06-hash-tables-maps-sets/08-symmetric-difference/symmetric-difference.js
@@ -1,3 +1,11 @@
-function symmetricDifference() {}
+function symmetricDifference(arr1, arr2) {
+  const set1 = new Set(arr1);
+  const set2 = new Set(arr2);
+  const diff = new Set([
+    ...arr1.filter((x) => !set2.has(x)),
+    ...arr2.filter((x) => !set1.has(x)),
+  ]);
+  return Array.from(diff);
+}
 
 module.exports = symmetricDifference;
--- 06-hash-tables-maps-sets/11-custom-hash-table/custom-hash-table-run.js
+++ 06-hash-tables-maps-sets/11-custom-hash-table/custom-hash-table-run.js
@@ -1,1 +1,12 @@
-const HashTable = require('./custom-hash-table');
+const HashTable = require("./custom-hash-table");
+
+const myHashTable = new HashTable();
+
+myHashTable.set("firstName", "John");
+myHashTable.set("lastName", "Doe");
+
+// console.log(myHashTable.get("firstName")); // John
+myHashTable.remove("firstName");
+console.log(myHashTable.get("firstName")); // undefined
+
+// myHashTable.printTable();
