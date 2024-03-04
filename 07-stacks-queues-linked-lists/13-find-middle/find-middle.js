963a774c2f75008a71602830cacd81fb9b5f6ada
Ramon-Carrillo <carrillojrramon@gmail.com>
2024-03-04 10:16:39 -0600

    finished challenges 13,15,16 of 07-stacks-queues-linked-lists

--- 07-stacks-queues-linked-lists/13-find-middle/find-middle.js
+++ 07-stacks-queues-linked-lists/13-find-middle/find-middle.js
@@ -1,3 +1,19 @@
-function findMiddle() {}
+function findMiddle(list) {
+  let slow = list.head;
+  let fast = list.head;
+  let prev = null;
+
+  while (fast !== null && fast.next !== null) {
+    prev = slow;
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+
+  if (fast === null) {
+    return prev.next;
+  } else {
+    return slow;
+  }
+}
 
 module.exports = findMiddle;
